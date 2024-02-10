24d665befd8ee6fd7b04b8a13d6127b8f052fd55
oleg.m <olegmarko7@gmail.com>
2024-02-10 04:14:43 +0100

    [+] Authentication

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,13 +3,37 @@
 namespace App\DataFixtures;
 
 use App\Entity\MicroPost;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    public function __construct(private readonly UserPasswordHasherInterface $userPasswordHasher) {}
+
     public function load(ObjectManager $manager): void
     {
+        $user1 = new User();
+        $user1->setEmail('test1@mail.com');
+        $user1->setPassword(
+            $this->userPasswordHasher->hashPassword(
+                $user1,
+                'password'
+            )
+        );
+        $manager->persist($user1);
+
+        $user2 = new User();
+        $user2->setEmail('test2@mail.com');
+        $user2->setPassword(
+            $this->userPasswordHasher->hashPassword(
+                $user2,
+                'password'
+            )
+        );
+        $manager->persist($user2);
+
         $microPost = new MicroPost();
         $microPost->setTitle('Post 1');
         $microPost->setText('Post 1 Text');
